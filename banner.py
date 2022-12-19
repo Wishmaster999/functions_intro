8c30ed7d325d1a01a638f11e551cc5ac721f5168
Andriy <wishmastour@gmail.com>
2022-12-19 22:11:34 +0200

    Initial commit

--- banner.py
+++ banner.py
@@ -29,3 +29,30 @@ print(result)
 
 numbers = [4, 2, 7, 5, 8, 3, 9, 6, 1]
 print(numbers.sort())
+
+
+def banner_text(text, screen_width = 88):
+    if len(text) > screen_width - 4:
+        print("EEK!!")
+        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+
+    if text == "*":
+        print("*" * screen_width)
+    else:
+        centred_text = text.center(screen_width - 4)
+        output_string = "**{0}**".format(centred_text)
+        print(output_string)
+
+
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If life seems jolly rotten,")
+banner_text("There's something you've forgotten!")
+banner_text("And that's to laugh and smile and dance and sing,")
+banner_text(" ", 70)
+banner_text("When you're feeling in the dumps,", 70)
+banner_text("Don't be silly chumps,", 70)
+banner_text("Just purse your lips and whistle - that's the thing!", 70)
+banner_text("And... always look on the bright side of life...", 70)
+banner_text("*", 70)
+
