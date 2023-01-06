5faff5028b6647ebf36ba313ad359aaf6efae6d3
evgenia <cagey_s@yahoo.com>
2023-01-06 22:27:26 +0300

    Initial commit

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    """Return the 'n' th Fibonacci number, for positive 'n'."""
+    if 0 <= n <= 1:
+        return n
+
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range (n -1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+for i in range (38):
+    print(i, fibonacci(i))
