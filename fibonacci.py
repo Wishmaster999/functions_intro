c8de1a8125753e3e026377de77aea8dec3ca6fbd
evgenia <cagey_s@yahoo.com>
2023-01-06 22:27:26 +0300

    Initial commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,23 @@
-def fibonacci(n):
+def multiply (x: float, y: float) -> float:
+    result = x * y
+    return result
+
+def is_palindrome (string: str) ->bool:
+    #backwards = string [::-1]
+    #return backwards == string
+    return string[::-1].casefold() == string.casefold()
+
+
+
+def palindrome_sentence (sentence: str) ->bool:
+    string = ""
+    for char in sentence:
+        if char.isalnum():  #using only alphanumeric symbols
+            string += char
+    return string[::-1].casefold() == string.casefold()
+
+
+def fibonacci(n: int) -> int:
     """Return the 'n' th Fibonacci number, for positive 'n'."""
     if 0 <= n <= 1:
         return n
@@ -15,3 +34,5 @@ def fibonacci(n):
 
 for i in range (38):
     print(i, fibonacci(i))
+
+p = palindrome_sentence()
