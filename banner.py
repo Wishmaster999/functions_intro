f4ec7c5586ecb9e6871e91d32552334ba15acf84
evgenia <cagey_s@yahoo.com>
2023-01-06 22:27:26 +0300

    Initial commit

--- banner.py
+++ banner.py
@@ -31,10 +31,20 @@ numbers = [4, 2, 7, 5, 8, 3, 9, 6, 1]
 print(numbers.sort())
 
 
-def banner_text(text = "*-*", screen_width = 88):
+def banner_text(text: str = "", screen_width: int = 88) -> None:
+    """ Print a string centred, with ** either side.
+
+   :param text: The string to print.
+       An asterisk (*) will result in a row of asterisks.
+       The default will print a blank line, with a ** border at
+       the left and right edges.
+   :param screen_width: The overall width to print within
+       (including the 4 spaces for the ** either side).
+   :raises ValueError: if the supplied string is too long to fit.
+   """
     if len(text) > screen_width - 4:
-        print("EEK!!")
-        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+        raise ValueError("String '{0}' is larger than specified width {1}"
+                         .format(text, screen_width))
 
     if text == "*":
         print("*" * screen_width)
